5aa1637515d6131a26fb7c75148bade3e9b867f6
Student <Student@tdc.edu.vn>
2021-03-30 09:00:20 +0700

    test

---
+++ app/Http/Controllers/AhihiController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AhihiController extends Controller
+{
+    //
+  
+    public function index(){
+        return view('welcome');
+    }
+    /*
+    public function index1($id="Không có ID"){
+        return 'In ra ở đây nè má:' .$id;
+    }
+    */
+    public function index1($id="welcome"){
+            return view($id);     
+    }
+}
---
+++ resources/views/sanpham.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Gio Hang</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+         
+    </head>
+    <body>
+        
+
+            <div class="content">
+                <div class="title m-b-md">
+                    Sản Phẩm
+                </div>
+
+                <div class="links">
+                    <a href="../public/welcome">Trang Chu   </a>
+                    <div class="btn-group">
+                        <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Sản Phẩm
+                        </button>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="../public/ban">Bàn </a>
+                            <a class="dropdown-item" href="../public/ghe">Ghế</a>
+                        </div>
+                    </div>
+                    
+                    <a href="../public/muasam">Mua Sam   </a>
+                    <a href="../public/giohang">Gio Hang   </a>
+                    <a href="../public/gioithieu">Gioi Thieu   </a>
+                </div>
+            </div>
+            <div class="container">
+            <!-- Example single danger button -->
+
+        </div>
+    </body>
+</html>
